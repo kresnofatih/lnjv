4d39ded36870aa39857342c2801b9f3a4af38401
Fatih <kresnofatihimani.private@gmail.com>
2021-01-24 11:29:55 +0700

    print execution

--- KTJavaBasic5/src/com/fatih/Main.java
+++ KTJavaBasic5/src/com/fatih/Main.java
@@ -3,5 +3,8 @@ package com.fatih;
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("Aku adalah Fatih");
+        System.out.print("baris keempat");
+        System.out.println("Cimam ke Jepang");
     }
 }
