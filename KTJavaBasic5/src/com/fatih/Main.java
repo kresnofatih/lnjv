e123cba140dbe6e25db47849f5917843b870ce15
Fatih <kresnofatihimani.private@gmail.com>
2021-01-24 11:46:26 +0700

    printf on screen

--- KTJavaBasic5/src/com/fatih/Main.java
+++ KTJavaBasic5/src/com/fatih/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Aku adalah Fatih");
-        System.out.print("baris keempat");
+        System.out.print("baris keempat\n");
         System.out.println("Cimam ke Jepang");
+        System.out.print("baris terakhir");
+        System.out.printf("Fatih umurnya %d", 22);
     }
 }
