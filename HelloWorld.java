b9a7337688c985f3069c174f7bb5b5d4a3730856
Fatih <kresnofatihimani.private@gmail.com>
2021-01-24 11:16:21 +0700

    created new directory for subproject

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!!\n");
         System.out.println("My Name is Fatih");
+        System.out.println("My Name is Fatih");
+        System.out.println("My Name is Fatih");
     }
 }
---
+++ KTJavaBasic5/src/programku/Program.java
@@ -0,0 +1,7 @@
+package programku;
+
+public class Program {
+    public static void main(String[] args){
+        System.out.println("Program");
+    }
+}
