50d6d0e1d2c6a13cdb962ce738ab8c062e75836e
Fatih <kresnofatihimani.private@gmail.com>
2021-01-26 00:07:16 +0700

    declaration of vars

--- KTJavaBasic5/src/com/fatih/Main.java
+++ KTJavaBasic5/src/com/fatih/Main.java
@@ -2,16 +2,21 @@ package com.fatih;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println(args[0]);
-        System.out.println(args[1]);
-        System.out.println("hello "+args[1]);
-        int apiKey = 5;
-        double numData = 4.0;
-        boolean marStatus = false;
-        char indexKardel = 'A';
-        System.out.println(apiKey);
-        System.out.println(numData);
-        System.out.println(marStatus);
-        System.out.println(indexKardel);
+        // create variables
+        int x = 10;
+        System.out.println("Nilai x = "+x);
+        x = 20;
+        System.out.println("Nilai x = "+x);
+        // System.out.println(args[0]);
+        // System.out.println(args[1]);
+        // System.out.println("hello "+args[1]);
+        // int apiKey = 5;
+        // double numData = 4.0;
+        // boolean marStatus = false;
+        // char indexKardel = 'A';
+        // System.out.println(apiKey);
+        // System.out.println(numData);
+        // System.out.println(marStatus);
+        // System.out.println(indexKardel);
     }
 }
