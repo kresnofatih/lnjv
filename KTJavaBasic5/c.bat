3a47d18a6f9202091e462dc9841d0354c1862a83
Fatih <kresnofatihimani.private@gmail.com>
2021-01-26 07:49:10 +0700

    arithmetic operations

--- KTJavaBasic5/c.bat
+++ KTJavaBasic5/c.bat
@@ -1,7 +1,7 @@
 rem To run this bat file, you need to be outside the bin and src file. if successful, class exe will be available in the bin folder. you are redirected to the initial loc.
 cd src
-javac com\fatih\Main.java -d ..\bin
+javac com\tutop\Main.java -d ..\bin
 cd ..\bin
 cls
-java com.fatih.Main
+java com.tutop.Main
 cd ..
---
+++ KTJavaBasic5/src/com/tutop/Main.java
@@ -0,0 +1,33 @@
+package com.tutop;
+
+public class Main {
+    public static void main(String[] args){
+        // arithmatic operations
+        int var1 = 5;
+        int var2 = 3;
+        int res1;
+        res1 = var1 + var2;
+        System.out.println(var1 + "+" + var2 + "=" + res1);
+        res1 = var1 - var2;
+        System.out.println(var1 + "-" + var2 + "=" + res1);
+        res1 = var1 * var2;
+        System.out.println(var1 + "x" + var2 + "=" + res1);
+        res1 = var1 / var2;
+        System.out.println(var1 + "/" + var2 + "=" + res1);
+        res1 = var1 % var2;
+        System.out.println(var1 + "%" + var2 + "=" + res1);
+        float var3 = 5.7f;
+        float var4 = 3.2f;
+        float res2;
+        res2 = var3 + var4;
+        System.out.println(var3 + "+" + var4 + "=" + res2);
+        res2 = var3 - var4;
+        System.out.println(var3 + "-" + var4 + "=" + res2);
+        res2 = var3 * var4;
+        System.out.println(var3 + "x" + var4 + "=" + res2);
+        res2 = var3 / var4;
+        System.out.println(var3 + "/" + var4 + "=" + res2);
+        res2 = var3 % var4;
+        System.out.println(var3 + "%" + var4 + "=" + res2);
+    }
+}
