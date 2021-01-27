25a1b2d6700ecbba630dde33e7339440d0fe2620
Fatih <kresnofatihimani.private@gmail.com>
2021-01-27 21:48:50 +0700

    input dgn scanner

--- KTJavaBasic5/src/com/compare/Main.java
+++ KTJavaBasic5/src/com/compare/Main.java
@@ -1,5 +1,7 @@
 package com.compare;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         int a, b;
@@ -15,5 +17,16 @@ public class Main {
         System.out.println("c OR d "+(c||d)); //or
         System.out.println("c AND d "+(c&&d)); //and
         System.out.println("c AND d "+(c^d)); //and
+
+        Scanner userInput = new Scanner(System.in);
+
+        int lebar = userInput.nextInt();
+        int panjang = userInput.nextInt();
+
+        //hitung luas persegipjg
+        // int panjang = 10;
+        // int lebar = 5;
+        int luas = panjang*lebar;
+        System.out.println(panjang+" "+lebar+" "+luas);
     }
 }
--- r.bat
+++ r.bat
@@ -1,4 +1,4 @@
 git add .
 git status
-git commit -m "operator komparasi logika"
+git commit -m "input dgn scanner"
 git push origin master
