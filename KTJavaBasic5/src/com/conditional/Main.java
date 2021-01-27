09df23f7df8f8de592017ac0d0bbc2c2603432bc
Fatih <kresnofatihimani.private@gmail.com>
2021-01-27 22:00:58 +0700

    if/elseif/else nested

--- KTJavaBasic5/src/com/conditional/Main.java
+++ KTJavaBasic5/src/com/conditional/Main.java
@@ -4,10 +4,25 @@ public class Main {
     public static void main(String[] args){
         int a = 10;
         if(a>6){
+            if(a%2==0){
+                System.out.println("genap");
+            } else {
+                System.out.println("ganjil");
+            }
             System.out.println("atas");
         } else if(a>2 && a<=6){
+            if(a%2==0){
+                System.out.println("genap");
+            } else {
+                System.out.println("ganjil");
+            }
             System.out.println("mid");
         }else {
+            if(a%2==0){
+                System.out.println("genap");
+            } else {
+                System.out.println("ganjil");
+            }
             System.out.println("bwh");
         }
     }
--- r.bat
+++ r.bat
@@ -1,4 +1,4 @@
 git add .
 git status
-git commit -m "if/elseif/else"
+git commit -m "if/elseif/else nested"
 git push origin master
